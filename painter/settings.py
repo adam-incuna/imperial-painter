1df57b8b4f41f03261388ce60d3126edf319c6d8
Adam Thomas <sortoflikechess@gmail.com>
2015-07-20 12:42:41 +0100

    The "real" initial commit - tons of basic setup.
    * Basic requirements, mostly dev-related.
    * Basic settings.
    * Coverage and flake8 configuration.
    * A makefile.
    * Empty URL and model files.
    * A test runner using the delightful colour-runner.

---
+++ painter/settings.py
@@ -0,0 +1,52 @@
+import os
+
+import dj_database_url
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DEBUG = bool(os.environ.get('DEBUG', False))
+
+INSTALLED_APPS = (
+    'django.contrib.contenttypes',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+SECRET_KEY = 'Django requires this to be set, but this project does not make use of it'
+
+ROOT_URLCONF = 'painter.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'painter.wsgi.application'
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgres://localhost/painter',
+    ),
+}
+DATABASES['default']['ATOMIC_REQUESTS'] = True
+
+LANGUAGE_CODE = 'en-gb'
+USE_TZ = False
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+STATIC_URL = os.environ.get('STATIC_URL', '/static/')
