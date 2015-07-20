aebb8329e791ee4d498056611b944c7704615728
Adam Thomas <sortoflikechess@gmail.com>
2015-07-20 17:30:11 +0100

    Add a get_template() method to Card.
    * The method ensures the template name begins with 'custom/'.

--- painter/models.py
+++ painter/models.py
@@ -10,3 +10,8 @@ class Card(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_template(self):
+        if self.template_name.startswith('custom/'):
+            return self.template_name
+        return 'custom/' + self.template_name
--- painter/tests/test_models.py
+++ painter/tests/test_models.py
@@ -24,3 +24,15 @@ class TestCard(TestCase):
         name = 'Leeroy Jenkins'
         card = factories.CardFactory.create(name=name)
         self.assertEqual(str(card), name)
+
+    def test_get_template(self):
+        """The method returns the template name if it has "custom/" already."""
+        template_name = 'custom/leeroy.html'
+        card = factories.CardFactory.create(template_name=template_name)
+        self.assertEqual(card.get_template(), template_name)
+
+    def test_get_template_adjustment(self):
+        """The method returns the template name with "custom/" added if necessary."""
+        template_name = 'leeroy.html'
+        card = factories.CardFactory.create(template_name=template_name)
+        self.assertEqual(card.get_template(), 'custom/' + template_name)
