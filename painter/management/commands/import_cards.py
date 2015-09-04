3661edd55553ff2dff27cb102a83d4751e033f2a
Adam Thomas <sortoflikechess@gmail.com>
2015-09-04 12:42:49 +0100

    Add help text and a 'filenames' argument.
    * Make it print the filenames it's receiving for the sake of
      good testing output.

--- painter/management/commands/import_cards.py
+++ painter/management/commands/import_cards.py
@@ -5,5 +5,18 @@ from painter.models import Card
 
 
 class Command(BaseCommand):
+    help = ('Clears the database of cards, then fills it with the contents of one or' +
+            ' more specified CSV files.')
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'filenames',
+            nargs='+',
+            type=str,
+            help='One or more CSV file names. The extension is optional.',
+        )
+
     def handle(self, *args, **options):
         dataset = tablib.Dataset()
+        for filename in options['filenames']:
+            print(filename)
