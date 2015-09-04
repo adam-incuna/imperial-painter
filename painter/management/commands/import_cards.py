02234b26787b92d2f073bfc3366bda7f18c5cb12
Adam Thomas <sortoflikechess@gmail.com>
2015-09-04 13:56:04 +0100

    Actually parse the CSV data into Python objects!
    * :D

--- painter/management/commands/import_cards.py
+++ painter/management/commands/import_cards.py
@@ -16,17 +16,39 @@ class Command(BaseCommand):
             help='One or more CSV file names. The extension is optional.',
         )
 
-    def ensure_csv_extension(self, filename):
-        """Add '.csv' onto the end of filename if it isn't already there."""
-        if filename.endswith('.csv'):
+    def ensure_extension(self, filename, extension):
+        """Add '.[extension]' onto the end of filename if it isn't already there."""
+        extension = '.' + extension
+        if filename.endswith(extension):
             return filename
-        return filename + '.csv'
+        return filename + extension
+
+    def safe_headers(self, headers):
+        """Replace spaces with underscores and make everything lowercase."""
+        return [
+            header.lower().replace(' ', '_')
+            for header in headers
+        ]
 
     def handle(self, *args, **options):
+        Card.objects.all().delete()
+
         for filename in options['filenames']:
-            filename = self.ensure_csv_extension(filename)
-            dataset = tablib.Dataset()
+            # Open the CSV file.
+            filename = self.ensure_extension(filename, 'csv')
             with open(filename, 'r') as csv_file:
                 file_contents = csv_file.read()
-                dataset.csv = file_contents
-                print(dataset.json)
+
+            # Load the CSV data and ensure it's safe for Python and template use.
+            dataset = tablib.Dataset()
+            dataset.csv = file_contents
+            dataset.headers = self.safe_headers(dataset.headers)
+            python_data = dataset.dict
+
+            # Create the cards!
+            for entry in python_data:
+                new_card = Card.objects.create(
+                    name=entry.pop('name'),
+                    template_name=self.ensure_extension(entry.pop('template'), 'html'),
+                    data=entry,  # the 'name' and 'template_name' have been removed, woo!
+                )
