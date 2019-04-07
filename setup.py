4e0b051d5dbec9d5cb6a6591a0b0478ba520b55e
Adam Thomas <adamthomas@e3d-online.com>
2019-04-07 20:45:18 +0100

    Fix MANIFEST.in syntax.

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
-import setuptools
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from setuptools import find_packages, setup
 
 setuptools.setup(
     name="imperial-painter",
     version="1.0.0",
     include_package_data=True,
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
 
     install_requires=[
         'django>=1.11.20',
@@ -22,8 +19,6 @@ setuptools.setup(
     author="Adam Thomas",
     author_email="sortoflikechess@gmail.com",
     description="A tool for generating prototype cards from Excel files and Django templates",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/adam-thomas/imperial-painter",
     classifiers=[
         "Programming Language :: Python :: 3",
